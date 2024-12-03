7d6746aca232c4901017ab1c4339fd6a5f0a0f36
juny429 <juny_0429@codeindesign.kr>
2024-12-03 15:12:59 +0900

    메인 이미지 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
+import Image from 'next/image';
 import Header from 'src/features/header';
 import Layout from 'src/features/Layout';
+import CrystalBallImg from '@/assets/images/crystal-ball.png';
 
 export default function Home() {
   return (
     <Layout>
       <Header />
+      <Image src={CrystalBallImg} alt="main image" width={200} height={200} />
     </Layout>
   );
 }
