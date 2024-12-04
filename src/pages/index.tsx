2dc30ad49bfefc47926d4f9ff5e23fd97307b0e7
juny429 <juny_0429@codeindesign.kr>
2024-12-04 11:48:40 +0900

    폴더명 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image';
 import CrystalBallImg from '@/assets/images/crystal-ball.png';
-import Header from 'src/features/header';
-import Layout from 'src/features/Layout';
-import MessageBox from 'src/features/MessageBox';
+import Header from 'src/components/header';
+import Layout from 'src/components/Layout';
+import MessageBox from 'src/components/MessageBox';
 
 export default function Home() {
   return (
