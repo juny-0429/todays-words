b6735d665fb3558e0acea69c0aa0f17bf3b0aec8
juny429 <juny_0429@codeindesign.kr>
2024-12-04 11:48:04 +0900

    메시지 박스 추가

---
+++ src/features/MessageBox.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function MessageBox() {
+  return (
+    <div className="flex flex-col justify-start px-5 py-2.5 border-solid border-2 rounded-lg border-mid-night-blue bg-beige shadow-md">
+      <time className="text-xs text-gray font-light">2024.12.25</time>
+      <p className="text-xm text-mid-night-blue font-normal">오늘은 크리스마스입니다. 사랑하는 사람들과 함께 행복한 하루를 보내요</p>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import Image from 'next/image';
+import CrystalBallImg from '@/assets/images/crystal-ball.png';
 import Header from 'src/features/header';
 import Layout from 'src/features/Layout';
-import CrystalBallImg from '@/assets/images/crystal-ball.png';
+import MessageBox from 'src/features/MessageBox';
 
 export default function Home() {
   return (
     <Layout>
       <Header />
       <Image src={CrystalBallImg} alt="main image" width={200} height={200} />
+      <MessageBox />
     </Layout>
   );
 }
