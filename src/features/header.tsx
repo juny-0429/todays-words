0a4c15f52dc63dc8b8c45e221160b72167a1d9d7
juny429 <juny_0429@codeindesign.kr>
2024-12-04 11:47:48 +0900

    컴포넌트 이름 대문자 수정

--- src/features/header.tsx
+++ src/features/header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { jalnan2 } from 'src/theme/fonts';
 
-export default function header() {
+export default function Header() {
   return (
     <header className="flex flex-col items-center gap-4">
       <h2 className="text-xs text-teal font-light">today&apos;s words</h2>
