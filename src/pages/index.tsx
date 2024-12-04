1ff94de6faf476e9144146ed7f352ad381989605
juny429 <juny_0429@codeindesign.kr>
2024-12-04 12:08:22 +0900

    버튼 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,13 @@ export default function Home() {
       <Header />
       <Image src={CrystalBallImg} alt="main image" width={200} height={200} />
       <MessageBox />
+
+      <div className="flex flex-col justify-center items-center gap-3">
+        {/* 행운 숫자 보기 */}
+        <button className="w-56 h-12 bg-teal rounded-full text-background text-lg font-semibold tracking-widest">행운 숫자 보기</button>
+        {/* 다른 사람 공유 */}
+        <button className="w-56 h-12 rounded-full border-2 border-solid border-teal text-teal text-lg font-semibold tracking-widest">다른 사람 공유</button>
+      </div>
     </Layout>
   );
 }
