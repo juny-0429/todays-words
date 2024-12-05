2f015e83e581eb156565d704249a2b4f91ff8d89
juny429 <juny_0429@codeindesign.kr>
2024-12-05 12:54:04 +0900

    행운 점수 제작

--- src/components/MessageBox.tsx
+++ src/components/MessageBox.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import Image from 'next/image';
+import CrystalBallImg from '@/assets/images/crystal-ball.png';
 
 export default function MessageBox() {
   return (
-    <div className="flex flex-col justify-start px-5 py-2.5 border-solid border-2 rounded-lg border-mid-night-blue bg-beige shadow-md">
-      <time className="text-xs text-gray font-light">2024.12.25</time>
-      <p className="text-xm text-mid-night-blue font-normal">오늘은 크리스마스입니다. 사랑하는 사람들과 함께 행복한 하루를 보내요</p>
+    <div className="flex flex-col items-center gap-2">
+      <Image src={CrystalBallImg} alt="main image" width={200} height={200} />
+      <div className="flex flex-col justify-start px-5 py-2.5 border-solid border  rounded-lg border-mid-night-blue bg-beige shadow-md">
+        <p>오늘의 한마디</p>
+        <time className="text-xs text-gray font-light">2024.12.25</time>
+        <p className="text-xm text-mid-night-blue font-normal">오늘은 크리스마스입니다. 사랑하는 사람들과 함께 행복한 하루를 보내요</p>
+      </div>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-import CrystalBallImg from '@/assets/images/crystal-ball.png';
 import Header from 'src/components/header';
 import Layout from 'src/components/Layout';
 import MessageBox from 'src/components/MessageBox';
@@ -9,7 +7,7 @@ export default function Home() {
   return (
     <Layout>
       <Header />
-      <Image src={CrystalBallImg} alt="main image" width={200} height={200} />
+
       <MessageBox />
       <LuckScore />
 
