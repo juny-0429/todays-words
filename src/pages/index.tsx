91dacc9d273446c637a0f2fba663e1ccf7fd8626
juny429 <juny_0429@codeindesign.kr>
2025-01-15 23:52:16 +0900

    점심 추천 페이지 제작

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,17 @@ import Link from 'next/link';
 export default function Home() {
   const menuList = [
     { label: '오늘의 해답', link: '/page1' },
-    { label: '점심 추천', link: '/page2' },
+    { label: '점심 추천', link: '/roulette/lunch' },
     { label: '행운 번호', link: '/page3' },
     { label: '하루 명언', link: '/page4' },
   ];
 
   return (
-    <main className="flex flex-col justify-center items-center w-auto h-screen">
+    <div className="flex flex-col justify-center items-center w-auto h-screen">
       <nav>
         <ul className="grid grid-cols-2 gap-9 gap-y-5">
           {menuList.map((menu) => (
-            <li key={menu}>
+            <li key={menu.label}>
               <Link href={menu.link} className="flex flex-col justify-center items-center gap-4">
                 <div className="w-32 h-32 rounded-3xl bg-gray"></div>
                 <span className="text-light-black text-xl font-medium">{menu.label}</span>
@@ -22,6 +22,6 @@ export default function Home() {
           ))}
         </ul>
       </nav>
-    </main>
+    </div>
   );
 }
