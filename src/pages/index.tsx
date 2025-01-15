f3626f0ac466c9aca53b5bc0f91183364b5c53a0
juny429 <juny_0429@codeindesign.kr>
2025-01-15 12:53:13 +0900

    메인 화면 제작

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,27 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <div></div>;
+  const menuList = [
+    { label: '오늘의 해답', link: '/page1' },
+    { label: '점심 추천', link: '/page2' },
+    { label: '행운 번호', link: '/page3' },
+    { label: '하루 명언', link: '/page4' },
+  ];
+
+  return (
+    <main className="flex flex-col justify-center items-center w-auto h-screen">
+      <nav>
+        <ul className="grid grid-cols-2 gap-9 gap-y-5">
+          {menuList.map((menu) => (
+            <li key={menu}>
+              <Link href={menu.link} className="flex flex-col justify-center items-center gap-4">
+                <div className="w-32 h-32 rounded-3xl bg-gray"></div>
+                <span className="text-light-black text-xl font-medium">{menu.label}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </main>
+  );
 }
